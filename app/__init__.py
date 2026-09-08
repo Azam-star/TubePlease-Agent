2c8b925a7384d8f46cf4e130319c268f1d01aa7c
Azam-star <basheeraazam02@gmail.com>
2026-09-08 09:58:39 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,17 @@
+import os , urlib.parse , urllib.request , render_template
+from app.youtube import youtuube_bp
+Gemini_api_key="Gemini API Key";
 
+def home():
+  return render_template ("indexx.html") # render_template is an function to call render which integrate in frontend
+
+def create_app():
+  app = Flask(_name_)
+  app.register_blueprint(youtube_bp, url_prefix="/youtube")
+
+@app.route("/html")
+
+def html():
+  return render_template("index.html")
+
+return app;
