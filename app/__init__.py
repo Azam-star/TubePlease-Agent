16db4be7183782712b821d8985abba3a8dd1229c
Azam-star <basheeraazam02@gmail.com>
2026-09-09 14:00:04 +0530

    Refactor app initialization and routes
    Refactor Flask app initialization and route definitions.

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,22 @@
-import os , urlib.parse , urllib.request , render_template
-from app.youtube import youtuube_bp
-Gemini_api_key="Gemini API Key";
+from flask import Flask, render_template
+from app.youtube import youtube_bp
 
-def home():
-  return render_template ("indexx.html") # render_template is an function to call render which integrate in frontend
 
 def create_app():
-  app = Flask(_name_)
-  app.register_blueprint(youtube_bp, url_prefix="/youtube")
 
-@app.route("/html")
+    app = Flask(__name__)
 
-def html():
-  return render_template("index.html")
+    app.register_blueprint(
+        youtube_bp,
+        url_prefix="/youtube"
+    )
 
-return app;
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    @app.route("/html")
+    def html():
+        return render_template("index.html")
+
+    return app
